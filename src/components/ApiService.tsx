75f7e27be095b5b3ea7b775853eb6eb5e0944490
camiexemplar <camilyvitoriaof@gmail.com>
2025-11-08 10:15:40 -0300

    chatbot & java URL

--- src/components/ApiService.tsx
+++ src/components/ApiService.tsx
@@ -1,5 +1,5 @@
 export const API_JAVA_URL =
-  import.meta.env.VITE_API_JAVA_URL || "http://localhost:8080";
+  import.meta.env.VITE_API_JAVA_URL || "https://carelink-api-f512.onrender.com/";
 
 export const API_CHAT_URL =
   import.meta.env.VITE_API_CHAT_URL || "https://carelink-bot.onrender.com";
